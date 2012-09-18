c040ea6f50cefe44e0b7f23d8af3bf5aec7cb701
Filipe Silva <d4rchangel@gmail.com>
2012-09-18 14:47:21 +0100

    Small bug correction

--- alpha_seq.py
+++ alpha_seq.py
@@ -4,32 +4,32 @@ import alpha_luhn
 def base10toN(num,n):
 	"""Change a to a base-n number.
 	Up to base-36 is supported without special notation."""
-	num_rep={10:'a',
-				11:'b',
-				12:'c',
-				13:'d',
-				14:'e',
-				15:'f',
-				16:'g',
-				17:'h',
-				18:'i',
-				19:'j',
-				20:'k',
-				21:'l',
-				22:'m',
-				23:'n',
-				24:'o',
-				25:'p',
-				26:'q',
-				27:'r',
-				28:'s',
-				29:'t',
-				30:'u',
-				31:'v',
-				32:'w',
-				33:'x',
-				34:'y',
-				35:'z'}
+	num_rep={10:'A',
+				11:'B',
+				12:'C',
+				13:'D',
+				14:'E',
+				15:'F',
+				16:'G',
+				17:'H',
+				18:'I',
+				19:'J',
+				20:'K',
+				21:'L',
+				22:'M',
+				23:'N',
+				24:'O',
+				25:'P',
+				26:'Q',
+				27:'R',
+				28:'S',
+				29:'T',
+				30:'U',
+				31:'V',
+				32:'W',
+				33:'X',
+				34:'Y',
+				35:'Z'}
 	new_num_string=''
 	current=num
 	while current!=0:
@@ -71,6 +71,7 @@ else:
 		else:
 			string = argv[3]
 	else:
+		string = argv[3]
 		symbol = argv[4]
 
 	generate_alphaseq(argv[1], argv[2], string, symbol)
